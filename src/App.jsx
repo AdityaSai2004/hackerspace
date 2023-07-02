40437ba0609f7e4bf78173460362016b5e432051
AdityaSai2004 <2004sai@gmail.com>
2023-07-02 21:40:53 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense, useEffect, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, useGLTF, useAnimations } from "@react-three/drei";
+import './App.css';
+import Scene from "./Scene";
+import Navbar from './components/navBar/nav'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Speaker from './components/speaker/Speakers'
+import Faq from './components/faq/faq'
+import Clock from './components/clock/Clock'
+import Sponsors from './components/sponsors/sponsors'
+import Footer from './components/footer/Footer'
+import About from './components/about/AboutPage'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="wrapper" style={{ width: "100vw", height: "100vh" }}>
+      {/* for now commenting the model */}
+      {/* <Canvas shadows={true} camera={{fov:100,position:[-1,1,4]}}> */}
+      {/* <Scene/> */}
+      {/* </Canvas> */}
+      {/* <Navbar currentPage="1" /> */}
+      {/* <Speaker /> */}
+      {/* <Faq /> */}
+      {/* <Clock /> */}
+      {/* <Sponsors /> */}
+      {/* <About /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<><Navbar currentPage='1' /><Footer urlb='/' urlf='/about'/></>} />
+          <Route path='about' element={<><Navbar currentPage='2' /><About /><Footer urlb='/' urlf='/speaker' /></>} />
+          <Route path='speaker' element={<><Navbar currentPage='3' /><Speaker /><Footer urlb='/about' urlf='/sponsors'/></>} />
+          <Route path='sponsors' element={<><Navbar currentPage='4' /><Sponsors /><Footer urlb='/speaker' urlf='/faq'/></>} />
+          <Route path='faq' element={<><Navbar currentPage='5' /><Faq /><Footer urlb='/sponsors' urlf='faq' /></>} />
+          <Route path='clock' element={<Clock />} />
+        </Routes>
+      </BrowserRouter>
+
+
+    </div>
+  );
 }
 
-export default App
+const View1 = () => {
+  <>
+    <Navbar currentPage='1'/>
+    <Speaker />
+    <Footer />
+  </>
+}
---
+++ src/components/sponsors/sponsors.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './sponsors.css'
+
+const Sponsors = () => {
+    return (
+        <>
+            
+            <div className='sponsors'>
+                <div className="span" id="title">SPONSORS</div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores vitae at quod soluta quam. Tempora illum sunt repellat doloribus accusamus.</p>
+
+                <div className="cards">
+                    <div className="box"> </div>
+                    <div className="box"> </div>
+                    <div className="box"> </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Sponsors
