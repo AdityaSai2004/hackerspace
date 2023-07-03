a7f5009c31765d2a89ce969dc1920b18846ad614
AdityaSai2004 <2004sai@gmail.com>
2023-07-03 21:02:10 +0530

    sponsor-rohan

--- src/components/sponsors/sponsors.jsx
+++ src/components/sponsors/sponsors.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import './sponsors.css'
+import React from "react";
+import "./sponsors.css";
 
 const Sponsors = () => {
-    return (
-        <>
-            
-            <div className='sponsors'>
-                <div className="span" id="title">SPONSORS</div>
-                <p>Our esteemed sponsors,who empower us to bring the world of cybersecurity to life at HackerSpace.</p>
+  return (
+    <>
+      <div className="sponsors">
+        <div className="span" id="title">
+          SPONSORS
+        </div>
+        <p>
+          Our esteemed sponsors,who empower us to bring the world of
+          cybersecurity to life at HackerSpace.
+        </p>
 
-                <div className="cards">
-                    <div className="box"> </div>
-                    <div className="box"> </div>
-                    <div className="box"> </div>
-                </div>
-            </div>
-        </>
-    )
-}
+        <h1 className="plat-spon">Platinum sponsors</h1>
+        <div className="cards">
+          <div className="box">
+            <img
+              src="src/components/sponsors/AECC.png"
+              alt="AECC"
+              className="images"
+            />
+          </div>
+          {/* <div className="box"> </div>
+                    <div className="box"> </div> */}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Sponsors
+export default Sponsors;
