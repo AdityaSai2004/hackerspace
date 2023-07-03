2f9cc95a64d5fc641ec3b6237750f431761de08c
AdityaSai2004 <2004sai@gmail.com>
2023-07-03 20:07:31 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,86 @@
 import React, { Suspense, useEffect, useRef } from "react";
-import './App.css';
-import Navbar from './components/navBar/nav'
+import "./App.css";
+import Navbar from "./components/navBar/nav";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Speaker from './components/speaker/Speakers'
-import Faq from './components/faq/faq'
-import Clock from './components/clock/Clock'
-import Sponsors from './components/sponsors/sponsors'
-import Footer from './components/footer/Footer'
-import About from './components/about/AboutPage'
+import Speaker from "./components/speaker/Speakers";
+import Faq from "./components/faq/faq";
+import Clock from "./components/clock/Clock";
+import Sponsors from "./components/sponsors/sponsors";
+import Footer from "./components/footer/Footer";
+import About from "./components/about/AboutPage";
 import StartPage from "./components/start/start";
-import Logo from './components/logo/logo'
+import Logo from "./components/logo/logo";
 
 export default function App() {
-  let logo = 'https://cyscomvit.com/assets/images/logo.png'
-  let title='HackerSpace'
-  let landingPageContent = "Welcome to HackerSpace, an extraordinary one-day workshop that will immerse you in the captivating realms of Bug Bounty, OSINT, and Cyber Forensics. This unique and hands-on cybersecurity event has been meticulously designed to equip participants with the latest knowledge and practical skills in these specialized domains."
+  let logo = "https://cyscomvit.com/assets/images/logo.png";
+  let title = "HackerSpace";
+  let landingPageContent =
+    "Welcome to HackerSpace, an extraordinary one-day workshop that will immerse you in the captivating realms of Bug Bounty, OSINT, and Cyber Forensics. This unique and hands-on cybersecurity event has been meticulously designed to equip participants with the latest knowledge and practical skills in these specialized domains.";
   return (
     <div className="wrapper" style={{ width: "100vw", height: "100vh" }}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<><Navbar currentPage='1' /><StartPage/><Footer urlb='/' urlf='/about'/></>} />
-          <Route path='about' element={<><Navbar currentPage='2' /><About name='About Event' content={landingPageContent}/><Footer urlb='/' urlf='/speaker' /></>} />
-          <Route path='speaker' element={<><Navbar currentPage='3' /><Speaker /><Footer urlb='/about' urlf='/sponsors'/></>} />
-          <Route path='sponsors' element={<><Navbar currentPage='4' /><Sponsors /><Footer urlb='/speaker' urlf='/faq'/></>} />
-          <Route path='faq' element={<><Navbar currentPage='5' /><Faq /><Footer urlb='/sponsors' urlf='/' /></>} />
-          <Route path='clock' element={<Clock />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar currentPage="1" />
+                <StartPage />
+                <Footer urlb="/" urlf="/about" />
+              </>
+            }
+          />
+          <Route
+            path="about"
+            element={
+              <>
+                <Navbar currentPage="2" />
+                <About name="About Event" content={landingPageContent} />
+                <Footer urlb="/" urlf="/speaker" />
+              </>
+            }
+          />
+          <Route
+            path="speaker"
+            element={
+              <>
+                <Navbar currentPage="3" />
+                <Speaker />
+                <Footer urlb="/about" urlf="/sponsors" />
+              </>
+            }
+          />
+          <Route
+            path="sponsors"
+            element={
+              <>
+                <Navbar currentPage="4" />
+                <Sponsors />
+                <Footer urlb="/speaker" urlf="/faq" />
+              </>
+            }
+          />
+          <Route
+            path="faq"
+            element={
+              <>
+                <Navbar currentPage="5" />
+                <Faq />
+                <Footer urlb="/sponsors" urlf="/" />
+              </>
+            }
+          />
+          <Route path="clock" element={<Clock />} />
         </Routes>
       </BrowserRouter>
-        
-
-
-
     </div>
   );
 }
 
 const View1 = () => {
   <>
-    <Navbar currentPage='1'/>
+    <Navbar currentPage="1" />
     <Speaker />
     <Footer />
-  </>
-}
+  </>;
+};
